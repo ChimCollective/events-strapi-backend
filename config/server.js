3d02d9d80d9a23de4cf778090888f10408b8bf30
Kato <kubkate@gmail.com>
2021-05-05 10:44:22 -0700

    backend starter files

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b94770b0a519515e2e93114687bdee6c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f920750c-f711-42ed-9af3-96778475ffab'
+};
